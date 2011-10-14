fc4fd9fc4aace029917c5389449a20cea8df398b
John Hilliard <praetoriansentry@gmail.com>
2011-10-13 19:50:19 -0700

    Initial Commit

---
+++ Password Maker/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Password Maker
+//
+//  Created by John Hilliard on 10/13/11.
+//  Copyright Self 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
